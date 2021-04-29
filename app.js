0e49709ac2bd12873f1ef9bf3c6d80c8320e132c
Kevin Mei <kevjinx@gmail.com>
2021-04-29 13:48:14 -0700

    mvp, but with lag

---
+++ app.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', event => {
+//mousedown = create element with setinterval at clientx and clienty
+//mouseup = remove all trail
+//set the setinterval inside of eventlistener or...
+//...put eventlistener
+  setInterval(() => {
+    document.addEventListener('mouseover', (e) => {
+      let x = e.clientX;
+      let y = e.clientY;
+      const trail = document.createElement('div');
+      trail.setAttribute('class', 'trail');
+      trail.style.top = `${y}px`;
+      trail.style.left = `${x}px`;
+      document.body.appendChild(trail)
+    })
+  }, 100);
+
+
+})
